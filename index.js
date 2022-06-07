81f898afcd39d51e23b00beb289b372b1e75f9d1
EmelKesten <kestenemel@gmail.com>
2022-06-07 19:32:01 +0200

    commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const input = document.querySelector("#input");
 const img = document.querySelector("#img");
+const weather = document.querySelector("#weather");
 const forecastButton = document.querySelector("#forecast");
+const todayButton = document.querySelector("#today");
 
 async function weatherToday(value) {
   const url = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=4d6f7c508a8274ee740483e4aab8ca29&units=metric `
   );
   const data = await url.json();
-  console.log(data);
-  const weather = document.querySelector("#weather");
   weather.innerHTML = `
       <h1>${data.name}</h1>
       <p>${data.weather[0].description}</p>
@@ -25,7 +25,6 @@ input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
     if (input.value) {
-      const weather = document.querySelector("#weather");
       weather.innerHTML = "";
       weatherToday(input.value);
     }
@@ -35,52 +34,36 @@ input.addEventListener("keypress", (event) => {
 forecastButton.addEventListener("click", (event) => {
   event.preventDefault();
   if (input.value) {
-    if (forecastButton.checked) {
-      forecastButton.checked = true;
-      const weather = document.querySelector("#weather");
-      weather.innerHTML = "";
-      forecast(input.value);
-    } else {
-      const weather = document.querySelector("#weather");
-      weather.innerHTML = "";
-      weatherToday(input.value);
-      forecastButton.checked = false;
-    }
-    if (forecastButton.checked) {
-      forecastButton.checked = true;
-    } else {
-      forecastButton.checked = false;
-    }
+    weather.innerHTML = "";
+    forecast(input.value);
   }
 });
+todayButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (input.value) {
+    weather.innerHTML = "";
+    weatherToday(input.value);
+  }
+});
+    
 
 async function forecast(value) {
   const url = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=4d6f7c508a8274ee740483e4aab8ca29&units=metric `
   );
   const data = await url.json();
-  console.log(data);
-  const forecast = document.querySelector("#weather");
   img.innerHTML = "";
-  forecast.innerHTML = `
+  weather.innerHTML = `
     <div class="forecastName">
       <h1>${data.city.name}</h1>
       <h2>Forecast</h2> 
     </div>
     <div class="forecastDiv">
-      ${data.list
-        .map(
-          (item) => `
+      ${data.list.map((item) => `
       <div class="forecast">
-      <li>${Math.round(
-        item.main.temp
-      )} °C </li><li><img src="http://openweathermap.org/img/wn/${
-            item.weather[0].icon
-          }@2x.png">      </li> <li>${item.dt_txt}</li>
-      </div>
-      `
-        )
-        .join("")}
-    </div>
-  `;
+      <li>${Math.round(item.main.temp)} °C </li>
+      <li><img src="http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png">      </li> 
+      <li>${item.dt_txt}</li>
+      </div>`).join("")}
+    </div>`;
 }
